adcc5cb063a34076e3ada52462a12d4c94928605
cyrille <cyrilllerotich@gmail.com>
2023-07-11 13:39:39 +0300

    style page

--- index.js
+++ index.js
@@ -13,6 +13,17 @@
                 newsDetails.id = "news"
                 const headline = document.createElement(`p`)
                 headline.innerHTML = data.title
+                headline.addEventListener(`click`,function(){
+                    window.open(data.url,"_blank")
+                    // fetch(`https://newsapi.org/v2/everything?q=bitcoin&apiKey=bb1faecdb871438096228c3a4e5d5ec5/${data.id}`)
+                    // .then(function(response){
+                    //   return  response.json()
+                    // })
+                    // .then(function(data){
+                    //     console.log(data)
+
+                    // })
+                })
                 const images = document.createElement(`img`)
                 images.src = data.urlToImage
                 newsDetails.appendChild(images)
