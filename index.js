b6c995781eb579fd36266ec3ea88306d24ddd272
cyrille <cyrilllerotich@gmail.com>
2023-07-11 12:52:16 +0300

    completed task

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-  const newsDetails = document.getElementById(`news details`)
+  const newscontainer = document.getElementById(`news details`)
   
+
   fetch(` https://newsapi.org/v2/everything?q=bitcoin&apiKey=bb1faecdb871438096228c3a4e5d5ec5`)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
-             data.forEach((articles) => {
+            console.log(data.articles);
+            const object = data.articles
+            object.forEach(data=> {
+                const newsDetails = document.createElement(`div`)
+                newsDetails.id = "news"
+                const headline = document.createElement(`p`)
+                headline.innerHTML = data.title
                 const images = document.createElement(`img`)
-                images.src = articles.urlToImage
+                images.src = data.urlToImage
                 newsDetails.appendChild(images)
+                newsDetails.appendChild(headline)
+                newscontainer.appendChild(newsDetails)
+
+
 
             });
         })
